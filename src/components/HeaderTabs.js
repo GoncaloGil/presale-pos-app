4896f0a9b8ad241d84451b53cb559d84291fb82d
GoncaloGil <goncalo.gil@hotmail.com>
2022-10-09 18:56:59 +0100

    feat: add blootstart classname to header buttons

--- src/components/HeaderTabs.js
+++ src/components/HeaderTabs.js
@@ -2,9 +2,9 @@ function HeaderTabs() {
   const tabs = ['Bebidas', 'Comidas', 'Diversos'];
 
   return (
-      <div className="row">
+      <div className="btn-group btn-group-lg" role="group" aria-label="Tabs Buttons">
         {tabs.map((tabItem) => (
-          <button>
+          <button type="button" class="btn btn-secondary">
             {tabItem}
           </button>
         ))}
